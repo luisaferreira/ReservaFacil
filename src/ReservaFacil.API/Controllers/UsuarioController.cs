9448000ce9017c83aae6232ace3e91bd6fa76be8
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2024-11-22 21:12:03 -0300

    Adicionado o projeto

---
+++ src/ReservaFacil.API/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReservaFacil.Data.Repositories;
+using ReservaFacil.Domain.Models;
+
+namespace ReservaFacil.API.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+
+        public UsuarioController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Inserir(Usuario usuario)
+        {
+            _usuarioRepository.Inserir(usuario);
+
+            return StatusCode(200);
+        }
+    }
+}
---
+++ src/ReservaFacil.Domain/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using ReservaFacil.Domain.Models.Shared;
+
+namespace ReservaFacil.Domain.Models
+{
+    public class Usuario : Base
+    {
+        public int IdPerfil { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Matricula { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
