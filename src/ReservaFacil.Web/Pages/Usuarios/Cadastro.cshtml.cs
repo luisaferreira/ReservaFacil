26b3065e5dba6fd6040290198c78e39438a2e016
Marina Flores <mlbtf@discente.ifpe.edu.br>
2024-11-23 18:26:19 -0300

    wip/cadastro de usuário: criação de menu lateral

---
+++ src/ReservaFacil.Web/Pages/Usuarios/Cadastro.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReservaFacil.Web.Pages.Usuarios;
+
+public class Cadastro : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public Cadastro(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
