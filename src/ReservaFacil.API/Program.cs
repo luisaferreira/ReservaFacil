ca48a825a2b24d5c5e8bf1443b271944a44df521
Marina Flores <mlbtf@discente.ifpe.edu.br>
2024-11-23 16:43:41 -0300

    feat(auth): adicionar autenticação JWT

--- src/ReservaFacil.API/Program.cs
+++ src/ReservaFacil.API/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        var chaveSeguranca = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("ReservaFacil_JwtSettings_Secret"));
+        var issuer = Environment.GetEnvironmentVariable("ReservaFacil_JwtSettings_Issuer");
+        var audience = Environment.GetEnvironmentVariable("ReservaFacil_JwtSettings_Audience");
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer, 
+            ValidateAudience = true,
+            ValidAudience = audience, 
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(chaveSeguranca)
+        };
+    });
+
+builder.Services.AddScoped<TokenHandler>();
 
 var app = builder.Build();
 
@@ -18,6 +46,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
