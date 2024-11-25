bd1089a1f35ca65037acdc3d6f34964deae79b46
Marina Flores <mlbtf@discente.ifpe.edu.br>
2024-11-24 21:24:11 -0300

    feat: front-end do cadastro de usuários

--- src/ReservaFacil.Web/Pages/Usuarios/Cadastro.cshtml.cs
+++ src/ReservaFacil.Web/Pages/Usuarios/Cadastro.cshtml.cs
@@ -1,17 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ReservaFacil.Web.Models;
 
 namespace ReservaFacil.Web.Pages.Usuarios;
 
 public class Cadastro : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public Cadastro(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
+    [BindProperty]
+    public UsuarioModel Usuario { get; set; }
+   
     public void OnGet()
     {
+    }
+
+    public IActionResult  OnPost()
+    {
+        try
+        {
+            string nome = Request.Form["Nome"];
+            string email = Request.Form["Email"];
+            string matricula = Request.Form["Matricula"];
+            string perfilId = Request.Form["PerfilId"];
+
+            var permissaoIds = Request.Form["PermissaoIds"]
+                .Select(id => Convert.ToInt32(id))
+                .ToList();
+
+            var usuario = new UsuarioModel(
+                nome,
+                email,
+                matricula,
+                perfilId,
+                permissaoIds
+            );
+            
+            foreach (var erro in Usuario.Errors)
+                ModelState.AddModelError(string.Empty, erro);
+            
+            if (!ModelState.IsValid)
+            {
+                StringBuilder erros = new StringBuilder();
+                foreach (var erro in usuario.Errors)
+                    erros.AppendLine(erro);
+        
+                TempData["ErrorMessage"] = erros.ToString();
+                return Page();
+            }
+
+            TempData["SuccessMessage"] = "Sua mensagem foi enviada com sucesso!";
+            return RedirectToPage();
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+            return Page();
+        }
     }
 }
