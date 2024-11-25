3be4805f1f9754aa1290b3de27d633f377b8182f
Marina Flores <48072489+Marina-Flores@users.noreply.github.com>
2024-11-25 09:19:28 -0300

    🚀 Adicionando front-end do cadastro de usuário (#2)
    * wip/cadastro de usuário: criação de menu lateral
    
    * wip - cadastro de usuário: criação de menu lateral
    
    * wip: menu lateral
    
    * wip: Tela de cadastro de usuário
    
    * feat: front-end do cadastro de usuários

---
+++ src/ReservaFacil.Web/Pages/Usuarios/Cadastro.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ReservaFacil.Web.Models;
+
+namespace ReservaFacil.Web.Pages.Usuarios;
+
+public class Cadastro : PageModel
+{
+    [BindProperty]
+    public UsuarioModel Usuario { get; set; }
+   
+    public void OnGet()
+    {
+    }
+
+    public IActionResult  OnPost()
+    {
+        try
+        {
+            string nome = Request.Form["Nome"];
+            string email = Request.Form["Email"];
+            string matricula = Request.Form["Matricula"];
+            string perfilId = Request.Form["PerfilId"];
+
+            var permissaoIds = Request.Form["PermissaoIds"]
+                .Select(id => Convert.ToInt32(id))
+                .ToList();
+
+            var usuario = new UsuarioModel(
+                nome,
+                email,
+                matricula,
+                perfilId,
+                permissaoIds
+            );
+            
+            foreach (var erro in Usuario.Errors)
+                ModelState.AddModelError(string.Empty, erro);
+            
+            if (!ModelState.IsValid)
+            {
+                StringBuilder erros = new StringBuilder();
+                foreach (var erro in usuario.Errors)
+                    erros.AppendLine(erro);
+        
+                TempData["ErrorMessage"] = erros.ToString();
+                return Page();
+            }
+
+            TempData["SuccessMessage"] = "Sua mensagem foi enviada com sucesso!";
+            return RedirectToPage();
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+            return Page();
+        }
+    }
+}
