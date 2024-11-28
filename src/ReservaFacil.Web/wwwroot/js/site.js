b8374f0d6a4b34a90d6da04bccf2799d9d0f89ae
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2024-11-28 19:35:51 -0300

    🚀 Adicionando front-end do listagem de usuário (#5)
    * wip: Iniciado ajustes no menu lateral
    
    * wip: Finalizada a estilização do menu lateral
    
    * wip: Implementada a tabela de listagem de usuários
    
    * wip: Adicionados os elementos e estilização de paginação
    
    * wip: Ajustes na organização do css
    
    * feat: Finalizada a troca de páginas pelo menu

--- src/ReservaFacil.Web/wwwroot/js/site.js
+++ src/ReservaFacil.Web/wwwroot/js/site.js
@@ -1,8 +1,21 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function voltar() {
+    window.history.back();
+}
 
-// Write your JavaScript code.
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-});
+function selecionarPagina(pagina) {
+    //Limpando o item ativo anteriormente
+    var item = $(".menu-item-active");
+    item.removeClass("menu-item-active");
+    var img = item.find("img");
+    var srcAntigo = img.attr("src");
+    srcAntigo = srcAntigo.replace("filled", "outline");
+    img.attr("src", srcAntigo)
+
+    //Selecionando o item novo
+    var imgSelecionado = $("img#" + pagina + "-icon").first();
+    var srcNovo = imgSelecionado.attr("src");
+    srcNovo = srcNovo.replace("outline", "filled");
+    imgSelecionado.attr("src", srcNovo);
+    var itemSelecionado = imgSelecionado.closest(".menu-item");
+    itemSelecionado.addClass("menu-item-active");
+}
