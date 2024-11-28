d73668f7e084f20f57edb97a98db7144ab721779
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2024-11-28 18:57:34 -0300

    feat: Finalizada a troca de páginas pelo menu

--- src/ReservaFacil.Web/wwwroot/js/site.js
+++ src/ReservaFacil.Web/wwwroot/js/site.js
@@ -1,3 +1,21 @@
 ﻿function voltar() {
     window.history.back();
 }
+
+function selecionarPagina(pagina) {
+    //Limpando o item ativo anteriormente
+    var item = $(".menu-item-active");
+    item.removeClass("menu-item-active");
+    var img = item.find("img");
+    var srcAntigo = img.attr("src");
+    srcAntigo = srcAntigo.replace("filled", "outline");
+    img.attr("src", srcAntigo)
+
+    //Selecionando o item novo
+    var imgSelecionado = $("img#" + pagina + "-icon").first();
+    var srcNovo = imgSelecionado.attr("src");
+    srcNovo = srcNovo.replace("outline", "filled");
+    imgSelecionado.attr("src", srcNovo);
+    var itemSelecionado = imgSelecionado.closest(".menu-item");
+    itemSelecionado.addClass("menu-item-active");
+}
