cc150d6153b0829a63f4f1358767f6107ae104e0
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2025-02-04 11:02:13 -0300

    Ajuste nos campos de períodos bloqueados

--- src/ReservaFacil.Web/wwwroot/js/views/Sala/cadastro.js
+++ src/ReservaFacil.Web/wwwroot/js/views/Sala/cadastro.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         if (horariosContainer && $(horarioTemplate).css('display') === 'none') {
             $(horarioTemplate).css('display', 'block');
             adicionaEventoDeRemoverHorario(horarioTemplate, true);
-            verificarCamposPreenchidos();
+            //verificarCamposPreenchidos();
             return;
         }
 
@@ -19,40 +19,12 @@ $(document).ready(function () {
 
         adicionaEventoDeRemoverHorario(novoHorario);
         atualizarCamposDeHorario(novoHorario, contaId);
-
+        
         horariosContainer.appendChild(novoHorario);
         contaId++;
-
-        verificarCamposPreenchidos();
     })
 
 })
-
-document.getElementById('horariosContainer').addEventListener('input', function (e) {
-    validaHorarioFinalMaiorQueInicial(e.target.name);
-    verificarCamposPreenchidos();
-});
-
-function validaHorarioFinalMaiorQueInicial(nomeElemento) {
-    const indexRegx = nomeElemento.match(/\[(\d+)]/);
-
-    if (indexRegx) {
-        const index = indexRegx[1];
-
-        const horarioInicial = document.querySelector(`input[name="HorarioInicial[${index}]"]`);
-        const horarioFinal = document.querySelector(`input[name="HorarioFinal[${index}]"]`);
-
-        if (horarioInicial && horarioFinal) {
-            const horarioInicialValue = new Date(horarioInicial.value);
-            const horarioFinalValue = new Date(horarioFinal.value);
-
-            if (horarioFinalValue <= horarioInicialValue) {
-                horarioFinal.value = "";
-                alert('O horário final deve ser maior que o horário inicial.');
-            }
-        }
-    }
-}
 
 function adicionaEventoDeRemoverHorario(horarioElemento, isDefault = false) {
     const removeButton = horarioElemento.querySelector('.btn-removeHorario');
@@ -62,32 +34,35 @@ function adicionaEventoDeRemoverHorario(horarioElemento, isDefault = false) {
                 horarioElemento.remove();
             } else {
                 $(horarioTemplate).css('display', 'none');
-                const horarioInicialInput = horarioElemento.querySelector(`input[name^="HorarioInicial"]`);
-                const horarioFinalInput = horarioElemento.querySelector(`input[name^="HorarioFinal"]`);
+                const horarioInicialInput = horarioElemento.querySelector(`input[name^="Periodo"]`);
+                const horarioFinalInput = horarioElemento.querySelector(`input[name^="Periodo"]`);
                 horarioInicialInput.value = "";
                 horarioFinalInput.value = "";
             }
-            verificarCamposPreenchidos();
         });
     }
+
+    $(`.txtPeriodo`).daterangepicker({
+        timePicker: true,
+        timePicker24Hour: true,
+        timePickerIncrement: 1,
+        locale: {
+            format: 'HH:mm'
+        }
+    }).on('show.daterangepicker', function (ev, picker) {
+        picker.container.find(".calendar-table").hide();
+    });
 }
 
 function atualizarCamposDeHorario(horarioElemento, id) {
-    const horarioInicialLabel = horarioElemento.querySelector(`label[for^="HorarioInicial"]`);
-    const horarioInicialInput = horarioElemento.querySelector(`input[name^="HorarioInicial"]`);
+    const periodoLabel = horarioElemento.querySelector(`label[for^="Periodo"]`);
+    const periodoInput = horarioElemento.querySelector(`input[name^="Periodo"]`);
 
-    const horarioFinalLabel = horarioElemento.querySelector(`label[for^="HorarioFinal"]`);
-    const horarioFinalInput = horarioElemento.querySelector(`input[name^="HorarioFinal"]`);
-
-    horarioInicialLabel.setAttribute('for', `HorarioInicial[${id}]`);
-    horarioInicialInput.setAttribute('name', `HorarioInicial[${id}]`);
-    horarioInicialInput.setAttribute('id', `HorarioInicial[${id}]`);
-    horarioFinalLabel.setAttribute('for', `HorarioFinal[${id}]`);
-    horarioFinalInput.setAttribute('name', `HorarioFinal[${id}]`);
-    horarioFinalInput.setAttribute('id', `HorarioFinal[${id}]`);
-
-    horarioInicialInput.value = "";
-    horarioFinalInput.value = "";
+    periodoLabel.setAttribute('for', `Periodo[${id}]`);
+    periodoInput.setAttribute('name', `Periodo[${id}]`);
+    periodoInput.setAttribute('id', `Periodo[${id}]`);
+    periodoLabel.setAttribute('for', `Periodo[${id}]`);
+    periodoInput.setAttribute('name', `Periodo[${id}]`);
 }
 
 function verificarCamposPreenchidos() {
